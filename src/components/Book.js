a79a3c61ce0c2a4e796d94cfcb36c4602c46a2be
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-05-17 12:57:27 +0500

    add action reducers for books | add and delete books

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,18 +2,39 @@
 import React from 'react';
 import './css/Book.css';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = ({ book }) => {
-  const { title, genre, author } = book;
+  const {
+    id, title, author, category,
+  } = book;
+  const dispatch = useDispatch();
+  const deleteHandler = () => {
+    dispatch(removeBook(id));
+  };
   return (
     <main className="main-box container">
       <div className="book">
-        <div className="genre">{genre}</div>
+        <div className="genre">{category}</div>
         <div className="title">{title}</div>
         <div className="author">{author}</div>
         <div className="btns-box">
-          <button className="action-btns" type="button">Comments</button>
-          <button className="action-btns" type="button">Remove</button>
+          <button
+            className="action-btns"
+            type="button"
+          >
+            Comments
+
+          </button>
+          <button
+            className="action-btns"
+            type="button"
+            onClick={deleteHandler}
+          >
+            Remove
+
+          </button>
           <button className="action-btns" type="button">Edit</button>
         </div>
       </div>
@@ -25,9 +46,10 @@ const Book = ({ book }) => {
 
 Book.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }),
 };
 
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,8 +1,17 @@
-import React from 'react';
+// import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-const Books = ({ books }) => books.map((book) => (
-  <Book book={book} key={book.id} />
-));
+// const Books = ({ books }) => books.map((book) => (
+//   <Book book={book} key={book.id} />
+// ));
+function Books() {
+  const books = useSelector((state) => state);
+
+  const list = books.reducer;
+  return list.map((book) => (
+    <Book book={book} key={book.id} />
+  ));
+}
 
 export default Books;
